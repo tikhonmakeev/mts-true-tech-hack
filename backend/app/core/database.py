6de79bb7ef1fbc20a0d856538089a292e9a3f768
truetecharena1744732281-user-637 <email@example.com>
2025-04-19 11:50:48 +0300

    made db sessions async

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,11 +1,9 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 import os
 
 DATABASE_URL = os.getenv(
     "DB_URL", "postgresql+asyncpg://user:password@db:5432/dbname")
 
 engine = create_async_engine(DATABASE_URL)
-SessionLocal = sessionmaker(
+SessionLocal = async_sessionmaker(
     engine, expire_on_commit=False, class_=AsyncSession)
-Base = declarative_base()
