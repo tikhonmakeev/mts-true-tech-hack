f035d58a6e289e3d8505b534b2de3f600cabad04
truetecharena1744732281-user-637 <email@example.com>
2025-04-21 03:19:53 +0300

    fixed migrations

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -8,6 +8,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from alembic import context
 
 from app.core.database import DATABASE_URL
+from app.orm_models.file import FileORM
 from app.orm_models.base import BaseORM
 
 # this is the Alembic Config object, which provides
